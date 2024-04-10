23b7db64d4cccd8d19f84b8b9f86b7281c2b3f92
Max Costa <maxxuelconta69@gmail.com>
2024-04-10 16:57:36 -0400

    Operadores

---
+++ Operadores/Atividade01.js
@@ -0,0 +1,8 @@
+const num1 = 20;
+const num2 =  2;
+console.log("seu numero é " + num1 + num2);
+console.log("seu numero é " + (num1 - num2));
+console.log("seu numero é " +num1 * num2);
+console.log("seu numero é " +num1 / num2);
+console.log("seu numero é " +num1 ** num2);
+console.log("seu numero é " +num1 & num2);
