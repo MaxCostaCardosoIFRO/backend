20016ac4d0d19f8638ef1bc6ad747d16fc7fbcdf
MaxCostaCardosoIFRO <159456189+MaxCostaCardosoIFRO@users.noreply.github.com>
2024-04-18 17:04:38 -0400

    Update Atividade02.js
    vscode bugou

---
+++ Objetos/Atividade02.js
@@ -0,0 +1,10 @@
+let produtoEmbalado = {
+
+    nome: "Laptop HP",
+    categoria: "Eletrônicos",
+    peso: 1.5,
+    preco: 3500.00,
+
+}
+
+console.log("O Prdotuo: " + produtoEmbalado.nome + " da categoria: "+ produtoEmbalado.categoria + " pesando: " + produtoEmbalado.peso +"KG "+ "esta a venda por: " + produtoEmbalado.preco + " reais." );
