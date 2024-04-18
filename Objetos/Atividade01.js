d96c3a12d843a1a359379988a86f9987717c6d34
MaxCostaCardosoIFRO <159456189+MaxCostaCardosoIFRO@users.noreply.github.com>
2024-04-17 20:32:16 -0400

    Update Atividade01.js
    teste

---
+++ Objetos/Atividade01.js
@@ -0,0 +1,9 @@
+let apartamento = {
+  quarto : 2,
+  tipo :" apartamento",
+  andar:  7 ,
+  endereço: "Avenida Principal, 456-Centro"
+
+}
+
+console.log("Apartamento com " + apartamento.quarto + " quartos"+ " localizado em "+ apartamento.endereço + "." );
